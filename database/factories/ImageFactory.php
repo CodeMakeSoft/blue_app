cd2faf01bd91c652ecf1c92d1ff9d696daa55071
E33-a <emax03736@gmail.com>
2025-07-09 22:07:10 -0600

    fix:ImageFactory descartado

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -9,25 +9,25 @@ use App\Models\Product;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Image>
  */
-class ImageFactory extends Factory
-{
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    // Associated model
-    protected $model  = Image::class;
+// class ImageFactory extends Factory
+// {
+//     /**
+//      * Define the model's default state.
+//      *
+//      * @return array<string, mixed>
+//      */
+//     // Associated model
+//     protected $model  = Image::class;
 
-    public function definition(): array
-    {
-        return [
-            // 'url' => 'https://picsum.photos/640/480', 
-            // // Id for related model
-            // 'imageable_id' => Product::factory(),
-            // // Stores the name of the model class
-            // 'imageable_type' => Product::class, 
-            'url' => 'https://picsum.photos/1024/1024?random=' . $this->faker->unique()->numberBetween(1, 1000),
-        ];
-    }
-}
+//     // public function definition(): array
+//     // {
+//     //     return [
+//     //         // 'url' => 'https://picsum.photos/640/480', 
+//     //         // // Id for related model
+//     //         // 'imageable_id' => Product::factory(),
+//     //         // // Stores the name of the model class
+//     //         // 'imageable_type' => Product::class, 
+//     //         'url' => 'https://picsum.photos/1024/1024?random=' . $this->faker->unique()->numberBetween(1, 1000),
+//     //     ];
+//     // }
+// }
